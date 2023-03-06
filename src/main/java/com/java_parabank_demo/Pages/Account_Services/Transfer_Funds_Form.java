9e102fd34ab72c03ee8870c44eed9dc34c5854cc
Samuil Getov <samuil.getov@securax.org>
2023-03-06 19:47:43 +0200

    Added new classes in Account_services (Transfer_Funds_Elements,Update_Contact_Info_Elements), new classes in Authorization (Authorization_Forms_Elements), changed config file content, changed the parsing way in CredentialsAndURLS

--- src/main/java/com/java_parabank_demo/Pages/Account_Services/Transfer_Funds_Form.java
+++ src/main/java/com/java_parabank_demo/Pages/Account_Services/Transfer_Funds_Form.java
@@ -1,60 +1,12 @@
 package com.java_parabank_demo.Pages.Account_Services;
 
-import com.java_parabank_demo.Pages.Authorization.Log_In_Form;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.time.Duration;
+public class Transfer_Funds_Form {
 
-public class Transfer_Funds_Form extends Log_In_Form {
-
-    @FindBy(how = How.XPATH, using = "//*[@id=\"leftPanel\"]/ul/li[3]/a")
-    @CacheLookup
-    WebElement transferFundLink;
-    @FindBy(how = How.XPATH, using = "//*[@id=\"amount\"]")
-    @CacheLookup
-    WebElement amountField;
-    @FindBy(how = How.ID, using = "fromAccountId")
-    @CacheLookup
-    WebElement fromAccountId;
-    @FindBy(how = How.ID, using = "toAccountId")
-    @CacheLookup
-    WebElement toAccountId;
-    @FindBy(how = How.XPATH, using = "//*[@id=\"rightPanel\"]/div/div/form/div[2]/input")
-    @CacheLookup
-    WebElement submitTransfer;
-
-    public Transfer_Funds_Form (WebDriver driver) {
-        super(driver);
-    }
-    public void checkTransferFunds () throws IOException, ParseException, org.json.simple.parser.ParseException, InterruptedException {
-
-        fillCredentials();
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        wait.until(ExpectedConditions.visibilityOf(transferFundLink));
-        transferFundLink.click();
-        Thread.sleep(1500);
-        wait.until(ExpectedConditions.elementToBeClickable(amountField));
-        amountField.sendKeys("150");
-
-        wait.until(ExpectedConditions.elementToBeClickable(fromAccountId));
-        fromAccountId.click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(toAccountId));
-        toAccountId.click();
-
-        wait.until(ExpectedConditions.visibilityOf(submitTransfer));
-        //Thread.sleep(1500);
-        submitTransfer.click();
-
-    }
+    public By transferFundsLinkText= By.xpath("//a[.='Transfer Funds']");
+    public By amountField= By.xpath("//input[@id='amount']");
+    public By fromAccField= By.xpath("//select[@id='fromAccountId']");
+    public By toAccField= By.xpath("//select[@id='toAccountId']");
+    public By transferButton= By.cssSelector("[type='submit']");
 }
--- src/main/java/com/java_parabank_demo/helpers/CredentialsAndURLS.java
+++ src/main/java/com/java_parabank_demo/helpers/CredentialsAndURLS.java
@@ -1,6 +1,5 @@
 package com.java_parabank_demo.helpers;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.junit.Test;
@@ -8,16 +7,15 @@ import org.junit.Test;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+
 public class CredentialsAndURLS {
 
     private static String baseURL;
-    public static String registerURL;
+    private static String registerURL;
     private static String users;
-    private static Long id;
     private static String firstName;
     private static String lastName;
     private static String address;
-    private static String city;
     private static String state;
     private static String zipCode;
     private static String phone;
@@ -26,37 +24,26 @@ public class CredentialsAndURLS {
     private static String password;
     private static String confirmPW;
 
-    public CredentialsAndURLS(){}; //class constructor
-
     public static void credentialsAndURLS() throws IOException, ParseException, org.json.simple.parser.ParseException {
-        FileReader reader = new FileReader("/home/tunay/java_parabank_demo/src/main/java/com/java_parabank_demo/config/config.json");
         JSONParser jsonparser = new JSONParser();
+        FileReader reader = new FileReader("C:/Users/Samuil/Desktop/Parabank/java_parabank_demo/src/main/java/com/java_parabank_demo/config/config.json");
         Object object = jsonparser.parse(reader);
         JSONObject configJsonObj = (JSONObject) object;
 
         baseURL = (String) configJsonObj.get("baseURL");
         registerURL = (String) configJsonObj.get("registerURL");
-
-        // Store the users array in a global variable
-        JSONArray usersArray = (JSONArray) configJsonObj.get("users");
-        users = usersArray.toString();
-        // Iterate over the users array and store the first user's data in global variables
-        for (Object user : usersArray) {
-            JSONObject userObj = (JSONObject) user;
-            id = (Long) userObj.get("id");
-            firstName = (String) userObj.get("firstName");
-            lastName = (String) userObj.get("lastName");
-            address = (String) userObj.get("address");
-            city = (String) userObj.get("city");
-            state = (String) userObj.get("state");
-            zipCode = (String) userObj.get("zipCode");
-            phone = (String) userObj.get("phone");
-            ssn = (String) userObj.get("ssn");
-            username = (String) userObj.get("username");
-            password = (String) userObj.get("password");
-            confirmPW = (String) userObj.get("confirmPW");
-            break;
-        }
+       // users = (String) configJsonObj.get("users"); //TODO
+        firstName = (String) configJsonObj.get("name");
+        lastName = (String) configJsonObj.get("lastName");
+        address = (String) configJsonObj.get("address");
+        state = (String) configJsonObj.get("state");
+        zipCode = (String) configJsonObj.get("zipcode");
+        phone = (String) configJsonObj.get("phone");
+        ssn = (String) configJsonObj.get("ssn");
+        username = (String) configJsonObj.get("username");
+        password = (String) configJsonObj.get("password");
+        confirmPW = (String) configJsonObj.get("confirmPW");
+       // confirmPW = configJsonObj.get("confirmPW").toString();
     }
 
     public void setBaseURL(String baseURL) {
@@ -66,26 +53,21 @@ public class CredentialsAndURLS {
     public static String getBaseURL() {
         return baseURL;
     }
-    public void setRegisterURL(String registerURL) {
-        this.registerURL = registerURL;
-    }
 
-    public static String getRegisterURL() {
-        return registerURL;
-    }
-    public void setUsers(String users) {
+    public void setUsers(String baseURL) {
         this.users = users;
     }
 
     public static String getUsers() {
         return users;
     }
-    public void setId(Long id) {
-        this.id = id;
+
+    public void setRegisterURL(String registerURL) {
+        this.registerURL = registerURL;
     }
 
-    public static Long getId() {
-        return id;
+    public static String getRegisterURL() {
+        return registerURL;
     }
 
     public void setFirstName(String firstName) {
@@ -111,74 +93,68 @@ public class CredentialsAndURLS {
     public static String getAddress() {
         return address;
     }
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public static String getCity() {
-        return city;
-    }
 
     public void setState(String state) {
         this.state = state;
     }
+
     public static String getState() {
         return state;
     }
+
     public void setZipCode(String zipCode) {
-        this.state = zipCode;
+        this.zipCode = zipCode;
     }
+
     public static String getZipCode() {
         return zipCode;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public static String getPhone() {
         return phone;
     }
+
     public void setSsn(String ssn) {
         this.ssn = ssn;
     }
+
     public static String getSsn() {
         return ssn;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
-    public static String getUsername() {
+
+    public String getUsername() {
         return username;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public static String getPassword() {
         return password;
     }
+
     public void setConfirmPW(String confirmPW) {
         this.confirmPW = confirmPW;
     }
+
     public static String getConfirmPW() {
         return confirmPW;
     }
+
     @Test
     public void test() throws IOException, ParseException, org.json.simple.parser.ParseException {
         credentialsAndURLS();
-        System.out.println("baseURL: " + baseURL);
-        System.out.println("registerURL: " + registerURL);
-        System.out.println("users: " + users);
-        System.out.println(id);
-        System.out.println("firstName: " + firstName);
-        System.out.println("lastName: " + lastName);
-        System.out.println("address: " + address);
-        System.out.println("city: " + city);
-        System.out.println("state: " + state);
-        System.out.println("zipCode: " + zipCode);
-        System.out.println("phone: " + phone);
-        System.out.println("ssn: " + ssn);
-        System.out.println("username: " + username);
-        System.out.println("password: " + password);
-        System.out.println("confirmPW: " + confirmPW);
+        System.out.println(baseURL);
+        System.out.println(registerURL);
+
     }
 }
-
