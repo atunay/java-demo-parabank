41656aac7f334525f6c4906c19f192fcc4d677a9
Zdravko Zafirov <zdravko.zafirov@gmail.com>
2023-03-10 23:41:45 +0200

    Add comments in files, modify .gitignore, remove unused files

--- src/main/java/com/java_parabank_demo/Pages/LoadTheWebsite.java
+++ src/main/java/com/java_parabank_demo/Pages/LoadTheWebsite.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import com.java_parabank_demo.helpers.CredentialsAndURLS;
 
 public class LoadTheWebsite {
-  String website = "https://parabank.parasoft.com/";
+  String website = "https://parabank.parasoft.com/"; // Replace it with the config base url
 
   public void LoadTheWebsite (WebDriver driver) {
     driver.get(website);
